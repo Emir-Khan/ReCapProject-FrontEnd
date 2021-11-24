4f26f2fa8c5a27a43160a8c173b5e3d221b3ea24
customoyuncu <kraloyuntv2@gmail.com>
2021-11-24 17:04:42 +0300

    Models

---
+++ src/app/models/card.ts
@@ -0,0 +1,6 @@
+export interface Cards{
+    id:number
+    cardNumber:string
+    cvv:string
+    expiryDate:Date
+}
---
+++ src/app/models/rental.ts
@@ -0,0 +1,7 @@
+export interface Rental {
+  userId: number;
+  carId: number;
+  carName: string;
+  rentDate: Date;
+  returnDate: Date;
+}
