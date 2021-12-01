067eec1dbfc18d3a76b99517a9bee3f51ab0492a
customoyuncu <kraloyuntv2@gmail.com>
2021-12-01 17:06:59 +0300

    New Payment Card

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup,FormControl,FormBuilder,Validator, Validators} from "@angular/forms"
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validator, Validators } from "@angular/forms"
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { CreditService } from 'src/app/services/credit.service';
@@ -12,45 +12,77 @@ import { RentalService } from 'src/app/services/rental.service';
 })
 export class PaymentComponent implements OnInit {
 
-  creditAddForm:FormGroup
+  creditAddForm: FormGroup
 
   constructor(
-    private formBuilder:FormBuilder,
-    private creditService:CreditService,
-    private rentService:RentalService,
-    private toastrService:ToastrService,
-    private route:Router
+    private formBuilder: FormBuilder,
+    private creditService: CreditService,
+    private rentService: RentalService,
+    private toastrService: ToastrService,
+    private route: Router,
+    private renderer: Renderer2,
+    private readonly elementRef: ElementRef
   ) { }
 
   ngOnInit(): void {
     this.createCardAddForm()
-  }
-
-  createCardAddForm(){
-    this.creditAddForm = this.formBuilder.group({
-      cardNumber:["",Validators.required],
-      password:["",Validators.required],
-      cvv:["",Validators.required],
-      expiryDate:["",Validators.required]
-    })
-  }
-
-  pay(){
-    console.log(this.creditAddForm.value)
     
-    if(this.creditAddForm.valid){
-      let cardModule = Object.assign({},this.creditAddForm.value)
-      let rentModule = Object.assign({},this.rentService.rent)
-      console.log(rentModule)
-      this.rentService.addRental(rentModule).subscribe(response=>{
-        this.toastrService.info(response.message,"Sistem")
-        this.toastrService.success("Ödeme Başarılı","Başarı")
-        this.route.navigate([""])
-      })
-      
-    }else{
-      this.toastrService.error("Bilgilerinizi doğru girdiğinizden emin olun","Hata")
+  }
+
+  createCardAddForm() {
+    this.creditAddForm = this.formBuilder.group({
+      cardNumber: ["", Validators.required],
+      password: ["", Validators.required],
+      cvv: ["", Validators.required],
+      expiryDate: ["", Validators.required]
+    })
+    this.load()
+  }
+
+  load(){
+    console.log("working")
+    const script = this.renderer.createElement('script');
+    script.src = 'assets/dist/js/payment.js';
+    script.onload = () => {
+      console.log('script loaded');
+
+    }
+    this.renderer.appendChild(document.querySelector("body"), script);
+  }
+
+  pay() {
+    console.log(this.creditAddForm.value)
+    let exp = new Date()
+
+    if (this.creditAddForm.valid == false) {
+      let cardModule = Object.assign({}, this.creditAddForm.value)
+      let rentModule = Object.assign({}, this.rentService.rent)
+
+      if (cardModule.expiryDate.length >= 6) {
+        cardModule.expiryDate = cardModule.expiryDate.slice(0, -1)
+      }
+      var lastTwo = cardModule.expiryDate.substr(cardModule.expiryDate.length - 2)
+      lastTwo = parseInt(lastTwo)
+      exp.setFullYear(lastTwo + 2000)
+      var firstTwo = cardModule.expiryDate.substr(0, 2)
+      exp.setMonth(firstTwo)
+      console.log(exp)
+      if (exp > new Date()) {
+        this.rentService.addRental(rentModule).subscribe(response => {
+          this.toastrService.info(response.message, "Sistem")
+          this.toastrService.success("Ödeme Başarılı", "Başarı")
+          this.route.navigate([""])
+        })
+      } else {
+        this.toastrService.error("Son Kullanım Tarihi Geçersiz")
+      }
+
+
+    } else {
+      this.toastrService.error("Bilgilerinizi doğru girdiğinizden emin olun", "Hata")
     }
   }
 
 }
+
+
