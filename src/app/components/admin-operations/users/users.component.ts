a502c45a75e72a02889b28cd6ef7afdfe9a420c4
customoyuncu <kraloyuntv2@gmail.com>
2021-12-03 17:02:54 +0300

    Users List For Admin Panel

---
+++ src/app/components/admin-operations/users/users.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  users:User[]
+
+  constructor(
+    private userService:UserService
+  ) { }
+
+  ngOnInit(): void {
+    this.getUsers()
+  }
+
+  getUsers(){
+    this.userService.getUsers().subscribe(response=>{
+      this.users=response.data
+    })
+  }
+
+  
+}
