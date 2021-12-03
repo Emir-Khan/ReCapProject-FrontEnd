9796dda16dfa45ec433fd7f2b4ef48ab050a7646
customoyuncu <kraloyuntv2@gmail.com>
2021-12-03 17:04:42 +0300

    Fix Was Selected

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Brand } from 'src/app/models/brand';
 import { Car } from 'src/app/models/car';
 import { Color } from 'src/app/models/color';
@@ -12,30 +13,49 @@ import { ColorService } from 'src/app/services/color.service';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
-  cars:Car[]
+  cars: Car[]
   colors: Color[]
   brands: Brand[]
-  colorId: number =-1 // ilk değere eşitlemek için "select option"
-  brandId: number =-1 // ilk değere eşitlemek için "select option"
+  colorId: number = -1
+  brandId: number = -1
 
   constructor(
-    private colorService:ColorService,
-    private brandService:BrandService,
-    private carService:CarService
+    private colorService: ColorService,
+    private brandService: BrandService,
+    private carService: CarService,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.getColors();
     this.getBrands();
+    this.activatedRoute.params.subscribe(params => {
+      if (params["colorId"] && params["brandId"]) {
+        this.colorId = params['colorId']
+        this.brandId = params['brandId']
+      }
+      else if (params["colorId"]) {
+        this.colorId = params["colorId"]
+        this.brandId = -1
+      }
+      else if (params["brandId"]) {
+        this.brandId = params['brandId']
+        this.colorId = -1
+      }
+      else {
+        this.colorId = -1
+        this.brandId = -1
+      }
+    })
   }
 
-  getColors(){
+  getColors() {
     this.colorService.getColors().subscribe(response => {
       this.colors = response.data
     })
   }
 
-  getBrands(){
+  getBrands() {
     this.brandService.getBrands().subscribe(response => {
       this.brands = response.data
     })
@@ -46,16 +66,16 @@ export class SideBarComponent implements OnInit {
     })
   }
 
-  setRouterLink(colorId:number,brandId:number){
-    if(colorId==-1&&brandId==-1){
+  setRouterLink(colorId: number, brandId: number) {
+    if (colorId == -1 && brandId == -1) {
       return ""
-    }else if(colorId==-1){
-      return "/cars/search/brand/"+brandId
-    }else if(brandId==-1){  
-      return "/cars/search/color/"+colorId
-    }else{
-      return "/cars/search/color/"+colorId+"/brand/"+brandId
+    } else if (colorId == -1) {
+      return "/cars/search/brand/" + brandId
+    } else if (brandId == -1) {
+      return "/cars/search/color/" + colorId
+    } else {
+      return "/cars/search/color/" + colorId + "/brand/" + brandId
     }
-    
+
   }
 }
